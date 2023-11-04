fef967faa77da7da4add5fecba3a8975e1f8b9f7
aron-alvarenga <aron.alvarenga@hotmail.com>
2023-11-04 16:26:09 -0400

    #5 Listando usuários cadastrados (index)

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -11,6 +11,35 @@
                 <div class="p-6 text-gray-900">
                     <p class="mb-4"> Olá <strong>{{ Auth::user()->name }}</strong></p>
                 </div>
+
+                <div class="p-6 text-gray-900">
+                    <div class="p-3 mb-4 bg-gray-200 rounded-lg">
+                        {{ $users->links() }}
+                    </div>
+                    <table class="w-full table-auto">
+                        <thead class="text-left bg-gray-200">
+                            <tr>
+                                <th class="text-center">Nível</th>
+                                <th class="p-4">Nome</th>
+                                <th>E-mail</th>
+                                <th>Data de cadastro</th>
+                                <th class="text-center">Ações</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            @foreach ($users as $user)
+                                <tr class="hover:bg-gray-200">
+                                    <td class="text-center">ícone</td>
+                                    <td class="p-2">{{ $user->name }}</td>
+                                    <td>{{ $user->email }}</td>
+                                    <td>{{ $user->created_at }}</td>
+                                    <td class="text-center"><a href="">Editar</a></td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
